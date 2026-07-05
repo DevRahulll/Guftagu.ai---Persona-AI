38b0bdbdd3debca95aa323c80f874498e0930790
Rahul <rahuldevan2019@gmail.com>
2026-07-05 23:11:55 +0530

    production deploy try-one

--- server/index.js
+++ server/index.js
@@ -4,20 +4,25 @@ import cors from "cors";
 import morgan from "morgan";
 import chatRouter from "./src/routes/chat.js";
 
+const PORT = process.env.PORT;
 const app = express();
-const PORT = 8080;
 
 app.use(express.json());
-app.use(cors());
+app.use(
+    cors({
+        origin: [
+            "https://guftagu-ai-persona-ai.vercel.app/",
+            "http://localhost:3000",
+        ],
+    }),
+);
 app.use(morgan("dev"));
 
 app.use("/api/chat", chatRouter);
 
 //health-check
 app.get("/health", (req, res) => {
-    res.json({
-        status: "OK! ALL GOOD",
-    });
+    res.send("All good");
 });
 
 app.listen(PORT, () => {
